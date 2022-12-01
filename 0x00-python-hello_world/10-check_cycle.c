c366f1cd8f235ec7eea486054a6d293dd71fe9e6
yohannes-y <111054083+yohannes-y@users.noreply.github.com>
2022-12-01 11:15:05 +0300

    Add files via upload

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+	return (0);
+}
