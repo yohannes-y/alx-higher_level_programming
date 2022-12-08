a80c2891c3dbb118366752afc7c26c73736d1e00
yohannes-y <yohannesy1672@gmail.com>
2022-12-08 09:29:05 +0000

    103-python.c

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size = (((PyVarObject *)(p))->ob_size);
+	Py_ssize_t allo = (((PyListObject *)(p))->allocated);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", allo);
+}
+
+void print_python_bytes(PyObject *p)
+{
+	(void) p;
+	printf("[.] bytes object info\n");
+}
