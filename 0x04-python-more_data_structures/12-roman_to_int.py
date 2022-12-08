a62efd43ba49a7e14bd14d95df9f2d31b0042f89
yohannes-y <yohannesy1672@gmail.com>
2022-12-08 09:23:28 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    result = 0
-    temp = list(roman_string)
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
+        roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                            'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        item = 0
+        roman = roman_string
+        if type(roman) is not str or len(roman) is 0:
+                return 0
+        for item in range(item, len(roman)):
+                if item < len(roman) - 1\
+                   and\
+                   roman_dictionary[roman[item]] <\
+                   roman_dictionary[roman[item + 1]]:
+                    result -= roman_dictionary[roman[item]]
+                else:
+                    result += roman_dictionary[roman[item]]
+        return result
